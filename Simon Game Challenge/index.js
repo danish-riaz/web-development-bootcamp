7d8bd75dc6cde5eff25d4d830917899d8d8022a5
Danish Riaz <danishriaz0609@outlook.com>
2020-03-30 17:30:32 +0500

    Simon Game Challenge, Done

--- Simon Game Challenge/index.js
+++ Simon Game Challenge/index.js
@@ -1,48 +1,38 @@
+var eventCounter = 0;
+var boxesName = ["green", "red", "yellow", "blue"];
 var level = 1;
-var userclickedBtns = [];
 
-var arr = ["green", "red", "yellow", "blue"];
-var gameClickedBtns = [];
+var gamePattern = [];
+var userPattern = [];
 
-// Starting game by pressing key
-$(document).bind("keypress", function(e) {
-  startGame();
+// Starting Point
+$(document).bind("keypress", function() {
+  showNextMove();
 });
-
-function randomHit() {
-  $("#level-title").text("Level " + level);
-  var randomNumber = Math.floor(Math.random() * 4) + 1;
-
-  //   adding and removing css
-  $("." + arr[randomNumber - 1]).addClass("pressed");
-  setTimeout(function() {
-    $("." + arr[randomNumber - 1]).removeClass("pressed");
-  }, 500);
-
-  //   Adding to gameBtns
-  gameClickedBtns.push($("." + arr[randomNumber - 1]).attr("id"));
-}
 
 $(".btn").click(function(e) {
-  userclickedBtns.push($(this).attr("id"));
+  ++eventCounter;
+  if (eventCounter == level) {
+    userPattern.push($(this).attr("id"));
+    if (JSON.stringify(gamePattern) == JSON.stringify(userPattern)) {
+      ++level;
+      showNextMove();
+    } else {
+      $("#level-title").text("You loose.Refresh to start again.");
+    }
+  } else {
+    userPattern.push($(this).attr("id"));
+  }
 });
 
-function startGame() {
-  gameBtns = [];
-  //   Changing Level
-  console.log("level in startGame " + level);
-  console.log("counter in startGame " + localCounter);
+function showNextMove() {
+  userPattern = [];
+  eventCounter = 0;
   $("#level-title").text("Level " + level);
-  var randomNumber = Math.floor(Math.random() * 4) + 1;
-
-  //   adding and removing css
-  $("." + arr[randomNumber - 1]).addClass("pressed");
+  var randomBoxSel = Math.floor(Math.random() * 4);
+  $("." + boxesName[randomBoxSel]).addClass("pressed");
   setTimeout(function() {
-    $("." + arr[randomNumber - 1]).removeClass("pressed");
-  }, 500);
-
-  //   Adding to gameBtns
-  gameBtns.push($("." + arr[randomNumber - 1]).attr("id"));
-
-  //   Detecting Mouse Click
+    $("." + boxesName[randomBoxSel]).removeClass("pressed");
+  }, 300);
+  gamePattern.push($("." + boxesName[randomBoxSel]).attr("id"));
 }
