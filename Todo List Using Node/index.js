1db1c463ce196225b3424b44e49d6ccc5842adb3
Danish Riaz <danishriaz0609@outlook.com>
2020-03-31 14:37:36 +0500

    Tudo logic, building complete

--- Todo List Using Node/index.js
+++ Todo List Using Node/index.js
@@ -2,12 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+const items = [];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get("/", function (req, res) {
-    res.render("index");
+    res.render("index", {
+        newlyAddedItem: items
+    });
+
 });
 
+app.post("/", function (req, res) {
+    items.push(req.body.newItem);
+    res.redirect("/");
+});
 
 app.listen(3000, function () {
     console.log("Server Running at port 3000.")
